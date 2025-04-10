e131e53e6852674bbd992cb22aa2f880a24d115c
Mabalod <johnkenneth0980>
2025-04-10 18:08:16 +0800

    crud

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CRUD APPLICATION IN PHP</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>3</td>
+                <td>John Kenneth</td>
+                <td>Roldan</td>
+                <td>19</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Mark Dave</td>
+                <td>Panaguiton</td>
+                <td>20</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
